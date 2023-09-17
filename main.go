a880c5fcfd445f6de10abe91f811ecfcc37704db
Qua Zi Xian <zixian1992@hotmail.com>
2023-09-17 13:26:07 +0000

    Tests web handler plugin using healthcheck plugin.

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"os/exec"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/ZiXian92/my-helper-bot/plugins"
+	"github.com/ZiXian92/my-helper-bot/plugins/interfaces"
 	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-plugin"
 	"github.com/rs/zerolog/log"
 )
 
@@ -21,6 +26,17 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Load plugins
+	// TODO: Implement a loop to load all enabled plugins found in the application's
+	// installed plugins folder.
+	healthCheckPluginClient := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig:  plugins.HandshakeConfig,
+		Plugins:          plugins.PluginMap,
+		Cmd:              exec.Command("./healthcheck"),
+		AllowedProtocols: []plugin.Protocol{plugin.ProtocolGRPC},
+	})
+	defer healthCheckPluginClient.Kill()
+
 	// Create a router using gorilla/mux
 	router := mux.NewRouter()
 	router.HandleFunc("/health", healthHandler)
@@ -30,6 +46,48 @@ func main() {
 		Addr:    ":8080",
 		Handler: router,
 	}
+
+	// Process plugins
+	healthCheckDispenser, err := healthCheckPluginClient.Client()
+	if err != nil {
+		log.Warn().Err(err).Msg("Failed to get healthcheck plugin dispenser.")
+	} else {
+		webHandlerPlugin, err := healthCheckDispenser.Dispense(plugins.PluginMapKeyWebHandler)
+		if err != nil {
+			log.Warn().Err(err).Msg("Failed to get dispense web handler plugin.")
+		} else if webHandler, ok := webHandlerPlugin.(interfaces.WebHandler); !ok {
+			log.Warn().Msg("Web handler plugin does not implement web handler interface.")
+		} else {
+			eps := webHandler.GetEndpoints()
+			log.Info().Any("web endpoints", eps).Msg("Found web endpoints.")
+			for _, ep := range eps {
+				router.HandleFunc(ep.Path, func(w http.ResponseWriter, r *http.Request) {
+					rBodyBytes, err := io.ReadAll(r.Body)
+					if err != nil {
+						log.Err(err).Msg("Error reading HTTP request body.")
+						http.Error(w, "Error processing request.", http.StatusInternalServerError)
+						return
+					}
+
+					res := webHandler.HandleRequest(interfaces.WebRequest{
+						EndPointName: ep.Name,
+						Headers:      r.Header,
+						URIParams:    mux.Vars(r),
+						QueryParams:  r.URL.Query(),
+						Body:         rBodyBytes,
+					})
+					for k, vList := range res.Headers {
+						for _, v := range vList {
+							w.Header().Add(k, v)
+						}
+					}
+					w.Write(res.Body)
+					w.WriteHeader(res.Code)
+				}).Methods(ep.Methods...)
+			}
+		}
+	}
+	defer healthCheckDispenser.Close()
 
 	go func() {
 		log.Info().Msg("Starting HTTP server")
